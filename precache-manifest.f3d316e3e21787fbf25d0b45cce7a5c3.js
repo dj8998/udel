72d85ac6dc2d90b85852a365ecf44716961148b8
dj8998 <63950335+dj8998@users.noreply.github.com>
2020-06-16 15:23:36 +0530

    Updates

---
+++ precache-manifest.f3d316e3e21787fbf25d0b45cce7a5c3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c45ff0250751989faf2f866a7d5dc77",
+    "url": "/udel/index.html"
+  },
+  {
+    "revision": "0c660b5f252e764d52b6",
+    "url": "/udel/static/css/2.d6351aec.chunk.css"
+  },
+  {
+    "revision": "d5d4a38f83c75d66451c",
+    "url": "/udel/static/css/main.0e09c8ed.chunk.css"
+  },
+  {
+    "revision": "9dafbbb669104c0f0a59",
+    "url": "/udel/static/js/10.15169bcf.chunk.js"
+  },
+  {
+    "revision": "a958690a24a78708b4c9",
+    "url": "/udel/static/js/11.e9c7b21b.chunk.js"
+  },
+  {
+    "revision": "fbcc8cf92a3afbac495d",
+    "url": "/udel/static/js/12.2f73490f.chunk.js"
+  },
+  {
+    "revision": "e7ccd0a62ea756d68a1a",
+    "url": "/udel/static/js/13.e53e7031.chunk.js"
+  },
+  {
+    "revision": "cc246d3155ec1d9ab53a",
+    "url": "/udel/static/js/14.615a7b15.chunk.js"
+  },
+  {
+    "revision": "0c660b5f252e764d52b6",
+    "url": "/udel/static/js/2.3895015a.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/udel/static/js/2.3895015a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1023f68f1a3bc77ae0",
+    "url": "/udel/static/js/3.cfa7b31f.chunk.js"
+  },
+  {
+    "revision": "bcc65dcdda9b7852c286",
+    "url": "/udel/static/js/4.65906738.chunk.js"
+  },
+  {
+    "revision": "dbd56cf25d7d7ef4097c",
+    "url": "/udel/static/js/5.4ab9f557.chunk.js"
+  },
+  {
+    "revision": "1ea065efa9deb44fcad8",
+    "url": "/udel/static/js/6.d843f6e8.chunk.js"
+  },
+  {
+    "revision": "06a57e811047ae284149",
+    "url": "/udel/static/js/7.4ce64fdf.chunk.js"
+  },
+  {
+    "revision": "32e8fddcf2ca9589f672",
+    "url": "/udel/static/js/8.6adb0954.chunk.js"
+  },
+  {
+    "revision": "eac187cd3e3ef7c42e25",
+    "url": "/udel/static/js/9.6465bd85.chunk.js"
+  },
+  {
+    "revision": "d5d4a38f83c75d66451c",
+    "url": "/udel/static/js/main.ebe2aa18.chunk.js"
+  },
+  {
+    "revision": "26b43318c49159e67057",
+    "url": "/udel/static/js/runtime-main.dff56092.js"
+  }
+]);
